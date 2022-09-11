aa04fb1dc883fb098735fb0fa9d5a06850d7cb70
Zbigniew Gołębiewski <zbigolo@gazeta.pl>
2022-09-11 23:45:40 +0200

    Drobne poprawki

--- src/PO_KlasaFile/KlasaFile.java
+++ src/PO_KlasaFile/KlasaFile.java
@@ -1,14 +1,16 @@
 package PO_KlasaFile;
+
 import java.io.File;
+
 public class KlasaFile {
 
     public static void main(String[] args) {
 
         File plik = new File("D:\\books.pdf");
 
-        System.out.println("Czy istnieje: "+ plik.exists());
-        System.out.println("Nazwa: "+ plik.getName());
-        System.out.println("PATH: "+ plik.getPath());
+        System.out.println("Czy istnieje: " + plik.exists());
+        System.out.println("Nazwa: " + plik.getName());
+        System.out.println("PATH: " + plik.getPath());
         System.out.println(plik.delete());
     }
 }
