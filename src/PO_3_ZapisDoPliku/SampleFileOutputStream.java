82b0c509d620a075861ab808d64dbb75c35c68a5
Zbigniew Gołębiewski <zbigolo@gazeta.pl>
2022-09-11 23:59:12 +0200

    zmiana nazw pakietów

---
+++ src/PO_3_ZapisDoPliku/SampleFileOutputStream.java
@@ -0,0 +1,32 @@
+package PO_2_ZapisDoPliku;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class SampleFileOutputStream {
+    public static void main(String[] args) throws IOException {
+        File plikdanych = new File("filmy.txt");
+        File plikkopii = new File("kopia.txt");
+
+        FileInputStream odczyt = null;
+        FileOutputStream zapis = null;
+
+        try{
+            odczyt = new FileInputStream(plikdanych);
+            zapis = new FileOutputStream(plikkopii);
+
+            int bit = 0;
+            while((bit = odczyt.read()) != -1){
+                zapis.write(bit);
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally {
+            if(odczyt != null) odczyt.close();
+            if(zapis != null) zapis.close();
+        }
+    }
+}
---
+++ src/PO_3_ZapisDoPliku/SamplePintWriter.java
@@ -0,0 +1,22 @@
+package PO_2_ZapisDoPliku;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SamplePintWriter {
+    public static void main(String[] args) throws IOException {
+
+        File savefile = new File("test.txt");
+        FileWriter file = new FileWriter(savefile);
+
+        String napis = "Ala ma kota i dwa psy.";
+        String[] words = napis.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            System.out.println(words[i]);
+            file.write(words[i]+"\n");
+        }
+        file.close();
+    }
+}
