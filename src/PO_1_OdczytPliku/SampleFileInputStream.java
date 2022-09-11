c066992463b293972e3acfac484c5b43eba0ef74
Zbigniew Gołębiewski <zbigolo@gazeta.pl>
2022-09-11 23:54:48 +0200

    Odczytywanie plików

---
+++ src/PO_1_OdczytPliku/SampleFileInputStream.java
@@ -0,0 +1,26 @@
+package PO_1_OdczytPliku;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+public class SampleFileInputStream {
+    public static void main (String[] args) throws IOException {
+        File plik = new File("filmy.txt");
+
+        FileInputStream odczyt = null;
+        try {
+            odczyt = new FileInputStream(plik);
+
+            int num = 0;
+            while ((num = odczyt.read()) != -1)
+                System.out.print((char) num);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (odczyt != null) odczyt.close();
+        }
+    }
+}
